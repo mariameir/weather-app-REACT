57263dcf7f885caec97b772d801366c677545dcc
Maria Meir <pigyarx@gmail.com>
2023-09-28 15:56:22 -0400

    teste

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './WeatherApp.css'
 
+import clear from '../Assets/clear.png'
+import cloud from '../Assets/cloud.png'
+import drizzle from '../Assets/drizzle.png'
+import humidity from '../Assets/clear.png'
+import rain from '../Assets/rain.png'
+import search from '../Assets/search.png'
+import snow from '../Assets/snow.png'
+import wind from '../Assets/wind.png'
+
 const WeatherApp = () => {
   return (
-    <div>WeatherApp</div>
+    <div className="container">
+      <div className="top-bar">
+        <input type="text" className="cityInput" placeholder='Pesquisa' />
+        <div className='search-icon'>
+            <img src={search} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
