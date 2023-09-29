a7eab1287e6f12e1cdb38865a60c8daa8a9d4118
Maria Meir <pigyarx@gmail.com>
2023-09-29 15:45:00 -0400

    container layout

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,26 +1,52 @@
-import React from 'react'
-import './WeatherApp.css'
+import React from "react";
+import "./WeatherApp.css";
 
-import clear from '../Assets/clear.png'
-import cloud from '../Assets/cloud.png'
-import drizzle from '../Assets/drizzle.png'
-import humidity from '../Assets/clear.png'
-import rain from '../Assets/rain.png'
-import search from '../Assets/search.png'
-import snow from '../Assets/snow.png'
-import wind from '../Assets/wind.png'
+import clear from "../Assets/clear.png";
+import cloud from "../Assets/cloud.png";
+import drizzle from "../Assets/drizzle.png";
+import humidity from "../Assets/humidity.png";
+import rain from "../Assets/rain.png";
+import search from "../Assets/search.png";
+import snow from "../Assets/snow.png";
+import wind from "../Assets/wind.png";
 
 const WeatherApp = () => {
   return (
     <div className="container">
       <div className="top-bar">
-        <input type="text" className="cityInput" placeholder='Pesquisa' />
-        <div className='search-icon'>
-            <img src={search} alt="" />
+        <input type="text" className="cityInput" placeholder="Pesquisa" />
+        <div className="search-icon">
+          <img src={search} alt="" />
         </div>
       </div>
-    </div>
-  )
-}
+      <div className="weather-image">
+        <img src={cloud} alt="" />
+      </div>
+      <div className="weather-temp">24°C</div>
+      <div className="weather-location">Brazil</div>
+      <div className="data-container">
 
-export default WeatherApp
+        <div className="element">
+          <img src={humidity} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">65%</div>
+            <div className="text">Umidade</div>
+          </div>
+        </div>
+
+        <div className="element">
+          <img src={wind} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">23km/h</div>
+            <div className="text">Vento</div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+
+);
+};
+
+export default WeatherApp;
